fb03da9b986e4f72218189e7f267ac2477d5d95c
rajbhowmik <rajbhowmik883@gmail.com>
2026-06-21 19:53:28 +0600

    V3+

--- Age Calc/Header files/functions.c
+++ Age Calc/Header files/functions.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include "../Function files/functions.h"
 
-int ly (int ty){ 
+int ly (int ty){  //leap year check
     if ((ty % 400 == 0) || (ty % 4 == 0 && ty % 100 != 0)){
         return 1;
     }
     return 0;
 }
 
-int vd(int td , int tm, int ty){ 
+int vd(int td , int tm, int ty){ //validity check
     if (td < 1||td > 31 || tm < 1 || tm > 12 || ty <1){
         printf("❌ Error: Invalid range! Day must be 1-31, Month 1-12, and Year positive.\n\n");
         return 0;
     }
-    if (tm == 2){ 
+    if (tm == 2){ //when 28 and when 29 check
         if (ly(ty) && td > 29){
             printf("❌ Error: %d is a Leap Year! February can't have more than 29 days.\n\n", ty);
             return 0;
@@ -23,14 +23,14 @@ int vd(int td , int tm, int ty){
             return 0;
         }
     }
-    if ((tm == 4 || tm == 6 || tm == 9 || tm == 11) && td > 30) { 
+    if ((tm == 4 || tm == 6 || tm == 9 || tm == 11) && td > 30) { //when 30 when 31 check
         printf("❌ Error: This month can't have more than 30 days.\n\n");
         return 0;
     }
     return 1;
 }
 
-int vdb(int bd, int bm, int by, int ty){ 
+int vdb(int bd, int bm, int by, int ty){ //birtday validity
     if (!vd (bd, bm, by)) {
         return 0;
     }
@@ -43,4 +43,67 @@ int vdb(int bd, int bm, int by, int ty){
         return 0;
     }
     return 1;
+}
+
+void next_birthday(int td, int tm, int ty, int bd, int bm) { //bday reminder
+    int next_y = ty;
+    int rem_m, rem_d;
+    int pm, dim;
+
+    if (bm < tm || (bm == tm && bd < td)) { //check if bday this year or not
+        next_y = ty + 1; 
+    }
+
+    if (bd == td && bm == tm) {
+        printf("\n🎉 Happy Birthday, Bhalo! Today is your special day! 🎂\n"); 
+        return;
+    }
+
+    if (bd >= td) { //if bday month is before or mext year
+        if (bm >= tm || next_y > ty) {
+            int target_m = (next_y > ty) ? bm + 12 : bm;
+            rem_m = target_m - tm;
+        }
+        rem_d = bd - td;
+    } 
+    else {
+        pm = bm - 1;
+        if (pm == 0) {
+            pm = 12; 
+        }
+
+        if (pm == 2) {
+            dim = ly(next_y) ? 29 : 28;
+        } else if (pm == 4 || pm == 6 || pm == 9 || pm == 11) {
+            dim = 30;
+        } else {
+            dim = 31;
+        }
+
+        rem_d = (bd + dim) - td; //previous day loan than
+        
+        int target_m = (next_y > ty) ? (bm - 1) + 12 : bm - 1; //? is shortcut to if else
+        rem_m = target_m - tm;
+    }
+
+    printf("\n⏳ Next Birthday Countdown:\n");
+    printf("Remaining: %d month(s) and %d day(s) until your next birthday!\n", rem_m, rem_d);
+}
+
+
+void print_birth_day_name(int bd, int bm, int by) {
+    int original_bm = bm; 
+    int original_by = by;
+
+    if (bm == 1) { bm = 13; by--; } //lemme tell you tdg a f*** how this works
+    if (bm == 2) { bm = 14; by--; }
+    
+    int q = bd;
+    int m = bm;
+    int k = by % 100;
+    int j = by / 100;
+    
+    int h = (q + 13*(m+1)/5 + k + k/4 + j/4 + 5*j) % 7; //zeller 
+    char days[][15] = {"Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+    printf("You were born on a: %s\n", days[h]);
 }
--- Age Calc/Main files/main.c
+++ Age Calc/Main files/main.c
@@ -3,61 +3,86 @@
 #include "../Function files/functions.h"
 
 int main (){
-    int td,tm,ty,bd,bm,by,d,m,y,pm,dim;
-    int check_vd = 0;
-    while (check_vd == 0) {
-        printf("Enter todays date (day/month/year)\n");
-        scanf("%d/%d/%d", &td, &tm, &ty);
-        
-        if (vd(td,tm,ty)) {
-            check_vd = 1;
-        }
-    }
-    
-    int check_vdb = 0;
-    while (check_vdb == 0) {
-        printf("Enter your birthday date (day/month/year)\n");
-        scanf("%d/%d/%d", &bd, &bm, &by);
-        
-        if (vdb(bd, bm, by, ty)) {
-            check_vdb = 1;
-        }
-     } 
+    char choice;
 
-    if (bd>td){
-        pm = tm -1;
-        if (pm ==0) { 
-            pm = 12; // for january
+    do {
+        int td, tm, ty, bd, bm, by, d, m, y, pm, dim;
+        int check_vd = 0;
+        
+        while (check_vd == 0) {
+            printf("\nEnter todays date (day/month/year)\n");
+            scanf("%d/%d/%d", &td, &tm, &ty);
+            
+            if (vd(td, tm, ty)) {
+                check_vd = 1;
+            }
         }
-        if (pm == 2) {
-            if (ly(ty)) { 
-                dim = 29;
+        
+        int check_vdb = 0;
+        while (check_vdb == 0) {
+            printf("Enter your birthday date (day/month/year)\n");
+            scanf("%d/%d/%d", &bd, &bm, &by);
+            
+            if (vdb(bd, bm, by, ty)) {
+                check_vdb = 1;
+            }
+         } 
+
+        
+        int temp_td = td; //to keep td... intact cuz we used them already the values are changed
+        int temp_tm = tm;
+        int temp_ty = ty;
+
+        if (bd > temp_td){
+            pm = temp_tm - 1;
+            if (pm == 0) { 
+                pm = 12; 
+            }
+            if (pm == 2) {
+                dim = ly(temp_ty) ? 29 : 28;
+            }
+            else if (pm == 4 || pm == 6 || pm == 9 || pm == 11) {
+                dim = 30;
             }
             else {
-                dim = 28;
+                dim = 31;
             }
+
+            temp_td = temp_td + dim; 
+            temp_tm = temp_tm - 1; 
         }
-        else if (pm == 4 ||pm == 6 ||pm == 9 ||pm == 11) {
-            dim = 30;
+        d = temp_td - bd;
+
+        if (bm > temp_tm){
+            temp_tm = temp_tm + 12; 
+            temp_ty = temp_ty - 1; 
         }
-        else {
-            dim = 31;
-        }
+        m = temp_tm - bm;
+        y = temp_ty - by;
 
-        td = td + dim; 
-        tm = tm-1; 
-    }
-    d = td-bd;
+        printf ("\nYour age is %d day %d month %d year \n", d, m, y);
 
-    if(bm>tm){
-        tm = tm+12; 
-        ty = ty-1; 
-    }
-    m=tm-bm;
+        
+        next_birthday(td, tm, ty, bd, bm);
 
-    y=ty-by;
+        
+        print_birth_day_name(bd, bm, by);
 
-    printf ("Your age is %d day %d month %d year \n",d,m,y);
+        
+        long int total_days = (y * 365) + (m * 30) + d; 
+        long int total_hours = total_days * 24;
+        long int total_minutes = total_hours * 60;
+
+        printf("\n--- Total Breakdown ---\n");
+        printf("Total Days: %ld\n", total_days);
+        printf("Total Hours: %ld\n", total_hours);
+        printf("Total Minutes: %ld\n", total_minutes);
+
+        
+        printf("\nDo you want to calculate another age? (y/n): ");
+        scanf(" %c", &choice); 
+
+    } while (choice == 'y' || choice == 'Y'); // y চাপলে লুপ আবার ঘুরবে
 
     return 0;
 }
